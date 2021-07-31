1ba4cf9513bba82325fa8104569ea98bac0e9d1a
Jason <jason_triche@rush.edu>
2021-07-31 08:42:28 -0500

    app.js edit 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. This has been modified.
         </p>
         <a
           className="App-link"
@@ -18,7 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <p>JT added this line</p>
     </div>
   );
 }
